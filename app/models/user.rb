0be68eb2f45e26a12a146168f6dc82321cd2df39
Victor Lin <victor@victorlinphoto.com>
2014-03-12 15:45:06 +0800

    Added bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+require 'bcrypt'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
   field :email, type: String
+  field :salt, type: String
+  field :fish, type: String
 end
