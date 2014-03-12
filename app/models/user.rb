aea36a794ef60a52c1079a2c4fcc936f95baf2e3
Victor Lin <victor@victorlinphoto.com>
2014-03-12 16:12:20 +0800

    Added password attr_accessor and salt and fish fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,21 @@ require 'bcrypt'
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  attr_accessor :password, :password_confirmation
+
   field :email, type: String
+
+  # User for encryption
   field :salt, type: String
   field :fish, type: String
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    puts "Encrypting Password: #{self.password}"
+    false
+    # false tells Mongoid not to save it.
+  end
+
 end
