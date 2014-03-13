177f5e189dbe983f8a0d9b05db309ff762615905
Victor Lin <victor@victorlinphoto.com>
2014-03-13 12:48:14 +0800

    Added Ruby Form Code

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,7 +3,6 @@ class SiteController < ApplicationController
   # index is a Ruby action aka a Ruby method, renders a view for us
   # corresponds to views/site/index.html.erb
   def index
-    @user = User.all.entries
   end
 
   def privacy
