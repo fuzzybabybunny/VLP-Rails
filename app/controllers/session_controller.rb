bcbd52947c9a9452272062ffaae11301e9da8adb
Victor Lin <victor@victorlinphoto.com>
2014-03-14 14:49:34 +0800

    Added logout messages using flash notice

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,25 +3,30 @@ class SessionController < ApplicationController
   # index is a Ruby action aka a Ruby method, renders a view for us
   # corresponds to views/site/index.html.erb
   def new
+    @messages = flash.map {|key, value| "#{key.capitalize}: #{value}"}.join(";")
     # render text: "Display the Login Form here."
   end
 
   # creates the session
   def create
+
     @user = User.authenticate(params[:user][:email], params[:user][:password])
+
+
     if @user
       # session is part of rails
       session[:user_id] = @user.id
       redirect_to root_url
     else
-      render text: "Incorrect Password"
+      render :new
     end
 
   end
 
   def destroy
     session[:user_id] = nil
-    render text: "Log the user out."
+    redirect_to login_url, notice: "You've successfully logged out."
   end
+
 
 end
